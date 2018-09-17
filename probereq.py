f0285321bd9a281b399a1adac1cbc22e1633b413
PAT <patrickett@protonmail.com>
2018-09-17 06:01:47 +0000

    Replace probereq.py

--- probereq.py
+++ probereq.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python2
-# Option to ignore gen-probe  /done
+# Option to ignore gen-probe does by default /done
 # identify devices based on MAC oui /done
 # write to a file contiuasly /done
 # option to use any interface /done
-# fix time updating
-
+# fix time updating /done
 # os.system('clear') #clears screen
 
 # pip install scapy + manuf +argparse
@@ -17,19 +16,13 @@ from time import gmtime, strftime
 from manuf import manuf #wireshark oui
 import argparse
 
-
 parser = argparse.ArgumentParser(description='Demo')
 parser.add_argument('--interface', '-i',required=True,help='Name of wireless interface in monitor mode' )
 args = parser.parse_args()
-
 iname = args.interface
 f = open('probes.txt', 'a')
-
 os.system('clear')
-
-time = strftime("%m/%d/%Y %H:%M:%S - ")
 broad = raw_input("Do you want broadcast probes? (Does not produce SSID) [Y/n]: ")
-
 yes = {'yes','y', 'ye', ''}
 no = {'no','n'}
 
@@ -44,10 +37,8 @@ if broad in yes :
                 ven = v.get_manuf(p.addr2)
                 if ssid == "":
                     ssid="gen-probe no SSID"
-                    print >> f, time + "[%s] - [%s] looking for --> [%s]" %(mac,ven,ssid)
-                    print time + "[%s] - [%s] looking for --> [%s]" %(mac,ven,ssid)
-
-#make sure no is still working
+                    print >> f, strftime("%m/%d/%Y %H:%M:%S - ") + "[%s] - [%s] looking for --> [%s]" %(mac,ven,ssid)
+                    print strftime("%m/%d/%Y %H:%M:%S - ") + "[%s] - [%s] looking for --> [%s]" %(mac,ven,ssid)
 
 if broad in no :
     print("Sniffing probe requests only with SSID... (This can take a bit)")
@@ -61,8 +52,8 @@ if broad in no :
                 if ssid == "":
                     sys.stdout.write('')
                 else :
-                    print >> f, time + " [%s] [%s] looking for --> [%s]" %(mac,ven,ssid)
-                    print time + " [%s] [%s] looking for --> [%s]" %(mac,ven,ssid)
+                    print >> f, strftime("%m/%d/%Y %H:%M:%S - ") + " [%s] [%s] looking for --> [%s]" %(mac,ven,ssid)
+                    print strftime("%m/%d/%Y %H:%M:%S - ") + " [%s] [%s] looking for --> [%s]" %(mac,ven,ssid)
 sniff(iface=iname,prn=proc)
 
 if KeyboardInterrupt:
